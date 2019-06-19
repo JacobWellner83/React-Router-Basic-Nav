7912aabba13ce0be87301f38a844084473de5239
JacobWellner83 <jcwellner83@gmail.com>
2019-06-19 12:00:04 -0500

    corrected Link declaration error

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,13 +7,13 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <Link href="">Home</Link>
+          <Link to="/">Home</Link>
         </div>
         <div>
-          <Link href="">About</Link>
+          <Link to="/about">About</Link>
         </div>
         <div>
-          <Link href="">Contact</Link>
+          <Link to="/Contact">Contact</Link>
         </div>
       </div>
     </div>
